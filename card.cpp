9716e1d1e09ccecd6beb9526b51228b8ddf3a041
Hamish Coleman <hamish@zot.org>
2024-09-07 13:36:07 +1000

    Add framework to store discovered card info strings

--- card.cpp
+++ card.cpp
@@ -8,6 +8,7 @@
 #include <Print.h>
 #include "card.h"
 #include "hexdump.h"
+#include "packets.h"
 
 void Card::set_uid(uint8_t *buf, uint8_t len) {
     uid_len = len;
@@ -32,4 +33,49 @@ void Card::print_uid(Print& p) {
 
     p.print('/');
     hexdump(p, uid, uid_len);
+}
+
+void Card::print_info(Print& p) {
+    switch(info_type) {
+        case INFO_TYPE_SERIAL_HSL:
+            p.print(F("hsl"));
+            break;
+        default:
+            p.print(F("ERROR"));
+            return;
+    }
+
+    p.print('/');
+    p.print(info);
+}
+
+void Card::print_info_msg(Print& p) {
+    if (info_type == INFO_TYPE_NONE) {
+        return;
+    }
+
+    packet_start(p);
+    switch(info_type & 0xf0) {
+        case INFO_TYPE_SERIAL_HSL:
+            p.print(F("serial"));
+            break;
+        default:
+            p.print(F("ERROR"));
+            return;
+    }
+    p.print('=');
+    print_info(p);
+    packet_end(p);
+}
+
+void Card::print_cardid(Print& p) {
+    p.print(F("cardid="));
+
+    if (info_type == INFO_TYPE_NONE) {
+        // Our best card ID is the UID
+        print_uid(p);
+        return;
+    }
+
+    print_info(p);
 }
--- card.h
+++ card.h
@@ -12,6 +12,10 @@
 #define UID_TYPE_ISO14443A  3
 #define UID_TYPE_FELICA     4
 
+#define INFO_TYPE_NONE          0
+#define INFO_TYPE_SERIAL        0x10
+#define INFO_TYPE_SERIAL_HSL    0x10
+
 #include <Print.h>
 
 class Card {
@@ -19,8 +23,15 @@ class Card {
         uint8_t uid_type;   // What card hardware type is represented
         uint8_t uid_len;    // How many bytes from uid are valid
         uint8_t uid[8];
+        uint8_t info_type;
+        char info[21];
 
-        Card(void) { uid_type=UID_TYPE_NONE; };
+        Card(void) {
+            uid_type=UID_TYPE_NONE;
+            info_type=INFO_TYPE_NONE;
+            // ensure that the info buf is zero-terminated
+            info[0] = 0;
+        };
 
         bool operator == (const Card &a) {
             if (uid_type != a.uid_type || uid_len != a.uid_len) {
@@ -35,5 +46,13 @@ class Card {
         void set_uid(uint8_t *buf, uint8_t len);
         void set_uid_type(uint8_t type) { uid_type=type; };
         void print_uid(Print& p);
+
+        void print_info(Print& p);
+
+        // Called before overwriting info[] to print any previous info
+        void print_info_msg(Print& p);
+
+        // Chooses the best string to represent this card and prints that
+        void print_cardid(Print& p);
 };
 
